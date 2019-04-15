1d80385857873ae5a8ca2dfa52e3ef39f111fe59
Gru <clement@redmoot.com>
2019-04-15 15:32:28 +0200

    FontAwesome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { PostListComponentComponent } from './post-list-component/post-list-component.component';
 import { PostListItemComponentComponent } from './post-list-item-component/post-list-item-component.component';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { PostListItemComponentComponent } from './post-list-item-component/post-
     PostListItemComponentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
